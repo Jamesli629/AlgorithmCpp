267b13ffbba843697d4ef4e9249738fbfab6605b
loveyu <libochao2020@163.com>
2022-03-13 16:22:59 +0800

    二分查找

--- Binary_Algorithm/Binary.cpp
+++ Binary_Algorithm/Binary.cpp
@@ -1,13 +1,42 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
-
+const int N=1e6;
+int n,key,arr[N];
 /**
  * 二分查找
  * 前提条件：数组要是有序的
  * 时间复杂度：O(logn)
  * 
- */ 
+ */
+int binarySearch(int arr[],int n,int key){//求等于key的最小下标
+    int l=1;
+    int r=n;
+    int ans=-1;
+    while(l<=r){
+        int mid=l+(r-l)/2;
+        if(arr[mid]==key){//==改成>=就是求第一个大于或等于key的下标（a）
+            ans = mid;    //接上：==改成>就是求第一个大于key的下标（b）
+            r=mid-1;      //求等于key值的个数：就可以用（a）式的值-（b）式的值
+        }
+        else{
+            l = mid + 1;
+        }
+    }
+    return ans;
+}
+bool cmp(int a,int b){
+    return a<b;        //从小到大排序
+}
 int main(){
-
+    cin>>n>>key;
+    for(int i=1;i<=n;i++)
+        cin>>arr[i];
+    sort(arr+1,arr+n+1,cmp);//防止输入不是有序的
+    for(int i=1;i<=n;++i){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+    int res = binarySearch(arr,n,key);
+    cout<<res<<endl;
     return 0;
 }
--- test/test.cpp
+++ test/test.cpp
@@ -1,10 +1,57 @@
-// 这是一个调试测试文件，您可以直接运行检测配置是否生效
-// 若配置任然无法调试，请查阅README_inProject.md
-
-#include <stdio.h>
-#include <stdlib.h>
-
-int main() {
-    printf("Congratulasions! It works!\n");
+#include<bits/stdc++.h>
+using namespace std;
+const int N=2e5+10;
+int arr[N];
+int n,c;
+int upBound(int key){
+    int l=0;
+    int r=n-1;
+    int ans=-1;
+    while(l<=r){
+        int m=l+(r-l)/2;
+        if(arr[m]>key){
+            ans = m;
+            r=m-1;
+        }
+        else{
+            l=m+1;
+        }
+    }
+    return ans;
+}
+int lowBound(int key){
+    int l=0;
+    int r=n-1;
+    int ans=-1;
+    while(l<=r){
+        int m=l+(r-l)/2;
+        if(arr[m]>=key){
+            ans = m;
+            r=m-1;
+        }
+        else{
+            l=m+1;
+        }
+    }
+    return ans;
+}
+int count(int num){
+    return upBound(num)-lowBound(num);
+}
+int main(){
+    long long ans = 0;
+    scanf("%d%d",&n,&c);
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    sort(arr,arr+n);
+    for(int i=0;i<n;i++)
+    {
+        int b=arr[i]-c;
+        long long res=count(b);
+        ans+=res;
+    }
+    cout<<ans;
     return 0;
 }
